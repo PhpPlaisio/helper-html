c5cbbf0ae2925504d7d8670e1470ca75378b474c
Paul Water <p.r.water@setbased.nl>
2021-01-14 13:09:34 +0100

    Prefer composition.

--- src/RenderWalker.php
+++ src/RenderWalker.php
@@ -9,42 +9,106 @@ namespace Plaisio\Helper;
 class RenderWalker
 {
   //--------------------------------------------------------------------------------------------------------------------
-  use CssModule;
+  /**
+   * The CSS module class.
+   *
+   * @var string
+   */
+  private string $moduleClass;
+
+  /**
+   * The CSS sub-module class.
+   *
+   * @var string|null
+   */
+  private ?string $subModuleClass;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * Object constructor.
    *
-   * @param string      $cssModuleClass    The CSS module class.
-   * @param string|null $cssSubModuleClass The CSS sub-module class.
+   * @param string      $moduleClass    The CSS module class.
+   * @param string|null $subModuleClass The CSS sub-module class.
    */
-  public function __construct(string $cssModuleClass, ?string $cssSubModuleClass = null)
+  public function __construct(string $moduleClass, ?string $subModuleClass = null)
   {
-    $this->moduleClass    = $cssModuleClass;
-    $this->subModuleClass = $cssSubModuleClass;
+    $this->moduleClass    = $moduleClass;
+    $this->subModuleClass = $subModuleClass;
   }
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * Returns the module, sub-module and sub-classes for an HTML element.
    *
-   * @param string|null $cssSubClass The CSS sub-class with the CSS module class.
+   * @param string|null $subClass The CSS sub-class with the CSS module class.
    *
    * @return string[]
    */
-  public function getClasses(?string $cssSubClass = null): array
+  public function getClasses(?string $subClass = null): array
   {
     $classes = [$this->moduleClass];
     if ($this->subModuleClass!==null)
     {
-      $classes[] = $this->cssSubModuleClass;
+      $classes[] = $this->subModuleClass;
     }
-    if ($cssSubClass!==null)
+    if ($subClass!==null)
     {
-      $classes[] = $this->moduleClass.'-'.$cssSubClass;
+      $classes[] = $this->moduleClass.'-'.$subClass;
     }
 
     return $classes;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the CSS module class.
+   *
+   * @return string
+   */
+  public function getModuleClass(): string
+  {
+    return $this->moduleClass;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the CSS sub-module class.
+   *
+   * @return string|null
+   */
+  public function getSubModuleClass(): ?string
+  {
+    return $this->subModuleClass;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets CSS module class.
+   *
+   * @param string $moduleClass The CSS module class.
+   *
+   * @return $this
+   */
+  public function setModuleClass(string $moduleClass): self
+  {
+    $this->moduleClass = $moduleClass;
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets CSS sub-module class.
+   *
+   * @param string|null $subModuleClass The CSS sub-module class.
+   *
+   * @return RenderWalker
+   */
+  public function setSubModuleClass(?string $subModuleClass): RenderWalker
+  {
+    $this->subModuleClass = $subModuleClass;
+
+    return $this;
   }
 
   //--------------------------------------------------------------------------------------------------------------------
